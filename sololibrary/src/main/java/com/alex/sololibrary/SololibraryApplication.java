0ec6244a2f59c7ffd19360394ad5cc1e404c49a8
alexgab <alexgabriel3357@gmail.com>
2023-10-06 03:09:50 -0300

    initial commit

---
+++ sololibrary/src/main/java/com/alex/sololibrary/SololibraryApplication.java
@@ -0,0 +1,13 @@
+package com.alex.sololibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SololibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SololibraryApplication.class, args);
+	}
+
+}
---
+++ sololibrary/src/test/java/com/alex/sololibrary/SololibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.sololibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SololibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
